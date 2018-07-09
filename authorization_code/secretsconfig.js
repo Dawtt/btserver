b28ff6c3298cccf606163197f2b391dfb4debe90
dw <davidwhitebird@gmail.com>
2018-07-09 00:29:06 -0500

    Initial commit

---
+++ authorization_code/secretsconfig.js
@@ -0,0 +1,5 @@
+const secretsconfigconst = {
+    client_id : 'de11a78ad2b94a5094ee825ef4088a5e', // Your client id
+    client_secret : '53f120e7b4c344289c41f89620d6ea93', // Your secret
+    redirect_uri : 'http://localhost:8888/callback', // Your redirect uri
+};
